a10853c4a83edca2cba4721f4ac7f822a01e7ab6
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-06-06 22:18:39 +0100

    updating files

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,30 +1,20 @@
 <?php 
     include("../db_connection.php");
-    // Start the session
-    session_start();
-    // Check if the session is not active or expired
-    if (!isset($_SESSION['adminEmail']) || time() > $_SESSION['expiration_time']) {
-        // Session expired, destroy the session and redirect to the login page
-        session_destroy();
-        header("Location: login.php");
-        exit();
-    }
-
-    // Update the expiration time to extend the session
-    $_SESSION['expiration_time'] = time() + 30 * 60;
-
+    include("session-config.php");
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bricoli</title>
+    <title>Bricoli | Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/admin-style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://kit.fontawesome.com/75c6b1327b.js" crossorigin="anonymous"></script>
   </head>
-  <body>
+  <body class="bg-pan-right">
     <header class="container-fluid">
         <nav class="navbar navbar-expand-lg bg-body-light">
             <div class="container-fluid">
@@ -69,21 +59,97 @@
                             <a class="nav-link" href="#">Inscription des bricoleurs</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Gestion de Réclamations</a>
+                            <a class="nav-link" href="#">Demande et Réclamations</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
 
-        <section class="container">
-            <div class="row row-cols-md-2 text-center">
-                <div class="col-12 border my-2">1</div>
-                <div class="col-12 border mb-2">2</div>
-                <div class="col-12 border mb-2">3</div>
-                <div class="col-12 border mb-2">4</div>
+        <section class="container my-5">
+            <div class="row row-cols-lg-2">
+                <div class="col-12 my-2">
+                    <a href="blog.php">
+                        <div class="card py-2">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-2 text-center">
+                                        <i class="fa-solid fa-newspaper articleIcon"></i>
+                                    </div>
+                                    <div class="col-6 text-start">
+                                        <h5 class="fs-1">Articles</h5>
+                                        <h6>Nombre total d'articles publiés</h6>
+                                    </div>
+                                    <div class="col-4 text-end">
+                                        <h2 class="fs-1">124</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-12 my-2">
+                    <div class="card py-2">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-2 text-center">
+                                    <i class="fa-solid fa-newspaper articleIcon"></i>
+                                </div>
+                                <div class="col-6 text-start">
+                                    <h5 class="fs-1">Bricoleurs</h5>
+                                    <h6>Bricoleurs inscrits</h6>
+                                </div>
+                                <div class="col-4 text-end">
+                                    <h2 class="fs-1">124</h2>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-12 my-2">
+                    <div class="card py-2">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-2 text-center">
+                                    <i class="fa-solid fa-newspaper articleIcon"></i>
+                                </div>
+                                <div class="col-6 text-start">
+                                    <h5 class="fs-1">Inscriptions</h5>
+                                    <h6>Inscriptions en attente de validation</h6>
+                                </div>
+                                <div class="col-4 text-end">
+                                    <h2 class="fs-1">124</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 my-2">
+                    <div class="card py-2">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-2 text-center">
+                                    <i class="fa-solid fa-newspaper articleIcon"></i>
+                                </div>
+                                <div class="col-6 text-start">
+                                    <h5 class="fs-1">Réclamations</h5>
+                                    <h6>Demandes et réclamations</h6>
+                                </div>
+                                <div class="col-4 text-end">
+                                    <h2 class="fs-1">124</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
             </div>
         </section>
+
+        
 
     </main>
     
